16f551c5493e81ab743c3fdc55010ea92db231b7
Andrés Córdoba Romero <jancordoba05@gmail.com>
2023-11-14 15:17:44 -0600

    Se agrega estilo a la barra de búsqueda. Se corrige código inncesario en app.js

--- src/App.js
+++ src/App.js
@@ -6,32 +6,12 @@ import ListPokemons from './components/ListPokemons';
 
 function App() { 
 
-  const [copyPokemons, setCopyPokemons] = useState([]);
   const [pokemons, setPokemons] = useState([]);
   const [input, setInput] = useState("");
 
   const url ='https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0';
-  
-  const filterPokemons = search => {
-    if(search.trim()){
-      search = search.trim();
-      const filteredPokemons = copyPokemons.filter((pokemon) => {
-        if(search === ''){
-          return pokemon;
-        }
-        else {
-          return pokemon.name.includes(search);  
-        }
-      });
-      setPokemons(filteredPokemons);
-    }
-    else {
-      setPokemons(copyPokemons);
-    }
-  };
 
   const changeText = (e) => {
-    console.log(e.target.value);
     setInput(e.target.value);
   };
 
@@ -39,7 +19,6 @@ function App() {
     axios.get(url)
       .then(response => {
         setPokemons(response.data.results);
-        setCopyPokemons(response.data.results);
       })
       .catch(error => {
         console.error(error);
@@ -49,7 +28,7 @@ function App() {
   return (
     <div className="App">
       <SearchBar 
-        onChange={filterPokemons} 
+        onChange={pokemons} 
         changeText={changeText} />
       <div className='cards-container'>
         <ListPokemons input={input} data={pokemons} />  
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
-import "../css/SearchBar.css";
+import React, { useState } from 'react';
+import '../css/SearchBar.css';
+import SearchIcon from '../images/search.png';
 
 function SearchBar(props) {
   return (
-    <div className="searchbar-container">
-      <input
-        className="searchbar-input"
-        type="text"
-        placeholder="Buscar pokemon"
-        name="searchbar-pokemon"
-        onChange={props.changeText}
-      />
-      <button className="searchbar-button">Buscar</button>
+    <div className='searchbar-container'>
+      <label>
+        <input
+          className='searchbar-input'
+          type='text'
+          placeholder='Buscar pokemon'
+          name='searchbar-pokemon'
+          onChange={props.changeText}
+          />
+        <img 
+          className='searchbar-button-icon' 
+          src={SearchIcon}/>
+      </label>
     </div>
   );
 }
