7077bb30e5c203d74c4ed3e04711ee00f400098f
Andrés Córdoba Romero <jancordoba05@gmail.com>
2023-11-14 16:33:46 -0600

    Se agrega los filtros para ver su forma masculina y femenina

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,56 @@
-import React from 'react';
-import '../css/Card.css'
-import shinyButtonOff from '../images/shiny-button-off.png';
-import shinyButtonOn from '../images/shiny-button-on.png';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../css/Card.css';
 import axios from 'axios';
-import pokemonNotFound from '../images/pokemonNotFound.png';
 import TypeColors from '../utils/TypeColors';
 import TypeIcons from '../utils/TypeIcons';
+import shinyButtonOff from '../images/shiny-button-off.png';
+import shinyButtonOn from '../images/shiny-button-on.png';
+import pokemonNotFound from '../images/pokemonNotFound.png';
+import male from '../images/male.png';
+import female from '../images/female.png';
 
 function Card ({name, url}) {
 
   const [isShiny, setIsShiny] = useState(false);
+  const [isMale, setIsMale] = useState(true)
   const [image, setImage] = useState('');
   const [imageShiny, setImageShiny] = useState('');
+  const [imageM, setImageM] = useState('');
+  const [imageShinyM, setImageShinyM] = useState('');
+  const [imageF, setImageF] = useState('');
+  const [imageShinyF, setImageShinyF] = useState('');
   const [type1, setType1] = useState('');
   const [type2, setType2] = useState('');
   
   const handleShinyButton = () => {
     setIsShiny(!isShiny);
   };
+  
+  const handleMaleButton = () => {
+    if(!isMale){
+      setIsMale(true);
+      setImage(imageM);
+      setImageShiny(imageShinyM);
+    } 
+  };
 
+  const handleFemaleButton = () => {
+    if(isMale){
+      setIsMale(false);
+      setImage(imageF);
+      setImageShiny(imageShinyF);
+    } 
+  };
+  
   useEffect(() => {
     axios.get(url)
       .then(response => {
         setImage(response.data.sprites.other.home.front_default?? pokemonNotFound);
         setImageShiny(response.data.sprites.other.home.front_shiny?? pokemonNotFound);
+        setImageM(response.data.sprites.other.home.front_default?? pokemonNotFound);
+        setImageShinyM(response.data.sprites.other.home.front_shiny?? pokemonNotFound)
+        setImageF(response.data.sprites.other.home.front_female?? response.data.sprites.other.home.front_default);
+        setImageShinyF(response.data.sprites.other.home.front_shiny_female?? response.data.sprites.other.home.front_shiny);
         setType1(response.data.types[0]?.type.name);
         setType2(response.data.types[1]?.type.name);
       })
@@ -40,14 +66,32 @@ function Card ({name, url}) {
     >
       <div className={'card-top'}>
         <div className='type-icon-container'>
-          <img
+        <img
             className='type-icon' 
-            src={TypeIcons[type1]} />
+            src={TypeIcons[type1]} 
+            alt={`type ${type1}`} />
+
+          { 
+            TypeIcons[type2] ?
+            <img 
+              className={'type-icon'}
+              src={TypeIcons[type2]} 
+              alt={`type ${type2 }` }/> 
+            : null
+          }
+          <img
+            className='gender-icon' 
+            src={male}
+            onClick={handleMaleButton}
+            alt='male icon' />
           
           <img 
-            className={TypeIcons[type2] ? 'type-icon' : ''}
-            src={TypeIcons[type2]?? null} />
+            className={'gender-icon'}
+            src={female} 
+            onClick={handleFemaleButton}
+            alt='female icon'/>
         </div>
+        
         <img 
             className='shiny-button'
             src={isShiny ? `${shinyButtonOn}` : `${shinyButtonOff}`}
